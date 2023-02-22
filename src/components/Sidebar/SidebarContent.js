5355be06291cc595d0065b921e27f090310fe509
nareshconnexials <naresh.sharma.connexials@gmail.com>
2023-02-22 15:59:53 +0530

    tooltip and tooltip feature with bg-color added

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -1,6 +1,15 @@
 import React from "react";
 import routes from "../../routes/sidebar";
-import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+
+import {
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import * as Icons from "../../icons";
 import { Button } from "@windmill/react-ui";
 import ThemedSuspense from "../ThemedSuspense";
@@ -14,14 +23,27 @@ function Icon({ icon, ...props }) {
 function SidebarContent(props) {
   const isRole = getRole("role");
   const navigate = useNavigate();
+  const location = useLocation();
 
+  console.log(routes.filter((i) => i.role.includes(isRole)));
   return (
-    <div className="py-4 text-gray-500 dark:text-gray-400">
+    <div
+      className={`py-4 text-gray-500 dark:text-gray-400 ${
+        props.active === true ? "w-full" : ""
+      }`}
+    >
       <ul className="mt-6">
         {routes
           .filter((i) => i.role.includes(isRole))
           .map((route) => (
-            <li className="relative px-6 py-3" key={route.name}>
+            <li
+              className={`relative px-6 py-3 ${
+                location.pathname === route.path
+                  ? "bg-purple-100 rounded-md"
+                  : ""
+              }`}
+              key={route.name}
+            >
               <NavLink
                 to={route.path}
                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 active:text-gray-800"
@@ -33,6 +55,9 @@ function SidebarContent(props) {
                   className="w-5 h-5"
                   aria-hidden="true"
                   icon={route.icon}
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content={route.name}
+                  data-tooltip-place="right"
                 />
                 {props.active ? (
                   <span className="ml-4">{route.name}</span>
@@ -59,6 +84,7 @@ function SidebarContent(props) {
           </Button>
         </div>
       )}
+      {props.active === false && <Tooltip id="my-tooltip" />}
     </div>
   );
 }
